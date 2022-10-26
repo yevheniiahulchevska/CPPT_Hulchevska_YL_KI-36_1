dd5be38e53843daeb8ec2511403e2500753e35e9
yevheniiahulchevska <111914021+yevheniiahulchevska@users.noreply.github.com>
2022-10-26 03:49:09 +0300

    Delete KI_36_Hulchevska_Lab6 directory

--- KI_36_Hulchevska_Lab6/CalculateException.java
+++
@@ -1,9 +0,0 @@
-package KI_36_Hulchevska_Lab6;
-
-public class CalculateException extends ArithmeticException {
-    public CalculateException() {
-    }
-    public CalculateException(String message) {
-        super(message);
-    }
-}
